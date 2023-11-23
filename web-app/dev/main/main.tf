5f5d70a79b12d53510ffd41e1456ffa5ddef39ac
giladk123 <giladk@gmail.com>
2023-11-23 22:32:54 +0200

    aaa

--- web-app/dev/main/main.tf
+++ web-app/dev/main/main.tf
@@ -1,6 +1,6 @@
 module "vnet" {
   source  = "app.terraform.io/hcta-azure-test/vnet/azurerm"
-  version = "11.0.0"
+  version = "12.0.0"
   # insert required Environment here.
   environment  = "dev"
 }
